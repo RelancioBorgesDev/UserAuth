19359dc7a73579dbe90295a23f25df75b0a85f98
RelancioBorgesDev <relancio.dev@gmail.com>
2024-01-22 14:41:27 -0300

    refactor: componente input ajustado

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,13 +1,28 @@
 import React, { InputHTMLAttributes } from "react";
+import { ForwardedRef } from "react";
+import { UseFormRegisterReturn } from "react-hook-form";
 
-interface InputProps extends InputHTMLAttributes<HTMLInputElement> {}
-
-export default function Input({ placeholder, ...rest }: InputProps) {
-  return (
-    <input
-      placeholder={placeholder}
-      {...rest}
-      className={` p-4 rounded-md border-2 border-zinc-600 bg-transparent outline-none text-white placeholder:text-gray-600`}
-    />
-  );
+interface InputProps extends InputHTMLAttributes<HTMLInputElement> {
+  name: string;
 }
+
+const Input = React.forwardRef(
+  (
+    { name, placeholder, ...rest }: InputProps,
+    ref: ForwardedRef<HTMLInputElement>
+  ) => {
+    return (
+      <input
+        {...rest}
+        name={name}
+        ref={ref}
+        placeholder={placeholder}
+        className={`p-4 rounded-md border-2 border-zinc-600 bg-transparent outline-none text-white placeholder:text-gray-00`}
+      />
+    );
+  }
+) as React.ForwardRefRenderFunction<HTMLInputElement, InputProps>;
+
+Input.displayName = "Input";
+
+export default Input;
--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -1,18 +1,42 @@
+"use client";
 import React from "react";
 import Separator from "../Separator/Separator";
 import SocialWrapper from "./components/SocialWrapper/SocialWrapper";
 import { FcGoogle } from "react-icons/fc";
 import { FaFacebook, FaXTwitter } from "react-icons/fa6";
 import Input from "../Input/Input";
+import * as z from "zod";
+import { useForm } from "react-hook-form";import { zodResolver } from "@hookform/resolvers/zod";
+interface LoginFormData {
+  email: string;
+  senha: string;
+}
+
+const LoginSchema: z.ZodType<LoginFormData> = z.object({
+  email: z.string().email(),
+  senha: z.string().min(4).max(12),
+});
 
 export default function LoginForm() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<LoginFormData>({
+    resolver: zodResolver(LoginSchema),
+  });
+
+  function handleSubmitLoginForm(data: LoginFormData) {
+    console.log(data);
+  }
   return (
     <form
       action=""
       className="w-[400px] bg-zinc-900 rounded-lg flex flex-col px-4 py-8 gap-8 shadow-2xl"
+      onSubmit={handleSubmit(handleSubmitLoginForm)}
     >
-      <Input placeholder="E-mail" type="email" />
-      <Input placeholder="Senha" type="senha" />
+      <Input placeholder="E-mail" type="email" {...register("email")} />
+      <Input placeholder="Senha" type="senha" {...register("senha")} />
       <button className="w-full bg-white p-2 font-bold text-slate-950 rounded border-0">
         Enviar
       </button>
