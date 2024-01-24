a0ffcbe42e765cfd25da596e4af5dfc960053340
RelancioBorgesDev <relancio.dev@gmail.com>
2024-01-24 12:02:02 -0300

    refactor: ajustado social icons

--- src/components/LoginForm/LoginForm.tsx
+++ src/components/LoginForm/LoginForm.tsx
@@ -3,10 +3,11 @@ import React from "react";
 import Separator from "../Separator/Separator";
 import SocialWrapper from "./components/SocialWrapper/SocialWrapper";
 import { FcGoogle } from "react-icons/fc";
-import { FaFacebook, FaXTwitter } from "react-icons/fa6";
+import { FaFacebook, FaGithub, FaXTwitter } from "react-icons/fa6";
 import Input from "../Input/Input";
 import * as z from "zod";
-import { useForm } from "react-hook-form";import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 interface LoginFormData {
   email: string;
   senha: string;
@@ -36,15 +37,25 @@ export default function LoginForm() {
       onSubmit={handleSubmit(handleSubmitLoginForm)}
     >
       <Input placeholder="E-mail" type="email" {...register("email")} />
+
       <Input placeholder="Senha" type="senha" {...register("senha")} />
       <button className="w-full bg-white p-2 font-bold text-slate-950 rounded border-0">
         Enviar
       </button>
       <Separator />
-      <div className="flex justify-around w-full">
-        <SocialWrapper link="/" icon={FcGoogle} color="" />
-        <SocialWrapper link="/" icon={FaXTwitter} color="text-white" />
-        <SocialWrapper link="/" icon={FaFacebook} color="text-blue-500" />
+      <div className="flex flex-col justify-around w-full gap-2">
+        <SocialWrapper
+          link="/"
+          icon={FcGoogle}
+          color=""
+          text="Faça login com o Google"
+        />
+        <SocialWrapper
+          link="/"
+          icon={FaGithub}
+          color="text-white"
+          text="Faça login com o Github"
+        />
       </div>
     </form>
   );
--- src/components/LoginForm/components/SocialWrapper/SocialWrapper.tsx
+++ src/components/LoginForm/components/SocialWrapper/SocialWrapper.tsx
@@ -4,18 +4,21 @@ interface SocialWrapperProps {
   link: string;
   icon: ElementType;
   color: string;
+  text: string;
 }
 export default function SocialWrapper({
   link,
   icon: Icon,
   color,
+  text,
 }: SocialWrapperProps) {
   return (
     <span
-      className={`border-2 border-slate-950 p-2 rounded-lg bg-zinc-950 ${color}`}
+      className={`border-2 border-slate-950 p-2 rounded-lg text-zinc-950 bg-white ${color} `}
     >
-      <Link href={link}>
+      <Link href={link} className="flex items-center justify-center gap-4">
         <Icon size={22} />
+        <p className="font-bold">{text}</p>
       </Link>
     </span>
   );
