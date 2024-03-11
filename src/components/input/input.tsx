9f9599975adb9b234ac4d1baf3bd7075dce09893
RelancioBorgesDev <relancio.dev@gmail.com>
2024-03-11 13:47:36 -0300

    feat: adicionado variante de erro no input

--- src/components/input/input.tsx
+++ src/components/input/input.tsx
@@ -1,21 +1,29 @@
-import React, { InputHTMLAttributes, forwardRef } from "react";
+import React, { ComponentProps, forwardRef } from "react";
 import { VariantProps, cva } from "class-variance-authority";
 import { cn } from "@/app/utils/cn";
 
 const inputVariants = cva("border-2 p-2 shadow-sm rounded outline-none", {
-  variants: {},
+  variants: {
+    variant: {
+      error: "border-red-500 border-4",
+    },
+  },
 });
 
 interface InputProps
-  extends InputHTMLAttributes<HTMLInputElement>,
+  extends ComponentProps<"input">,
     VariantProps<typeof inputVariants> {}
 
 const Input = forwardRef<HTMLInputElement, InputProps>(function Input(
-  { className, ...props }: InputProps,
+  { className, variant, ...props }: InputProps,
   ref
 ) {
   return (
-    <input ref={ref} className={cn(inputVariants({ className }))} {...props} />
+    <input
+      ref={ref}
+      className={cn(inputVariants({ className, variant }))}
+      {...props}
+    />
   );
 });
 
