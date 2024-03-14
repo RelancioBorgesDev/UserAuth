5adc6f502da83c93b81f1968c99ff7c5865dcc69
RelancioBorgesDev <relancio.dev@gmail.com>
2024-03-14 14:21:46 -0300

    feat: adicionado dropdown no avatar do header

--- src/app/(private)/_components/dashboard-header/components/header-avatar/header-avatar.tsx
+++ src/app/(private)/_components/dashboard-header/components/header-avatar/header-avatar.tsx
@@ -2,15 +2,12 @@ import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 
 export default function HeaderAvatar() {
   return (
-    <div className="text-white flex hover:bg-gray-600/25 rounded p-2 gap-4">
+    <div className="text-white flex hover:bg-gray-600/25 rounded p-2 gap-4 w-full">
       <Avatar>
         <AvatarImage src="https://github.com/RelancioBorgesDev.png" />
         <AvatarFallback>CN</AvatarFallback>
       </Avatar>
-      <div>
-        <h4>{"Relancio Borges"}</h4>
-        <h5>@{"RelancioBorgesDev"}</h5>
-      </div>
+      
     </div>
   );
 }
--- src/app/(private)/_components/dashboard-header/dashboard-header.tsx
+++ src/app/(private)/_components/dashboard-header/dashboard-header.tsx
@@ -1,9 +1,26 @@
 import { Input } from "@/components/input/input";
 import Logo from "@/components/logo/logo";
-import { Bell, Moon, Search } from "lucide-react";
+import {
+  Bell,
+  LogOut,
+  MessageCircleQuestion,
+  Moon,
+  Search,
+  Settings,
+  User,
+} from "lucide-react";
 import React from "react";
 import HeaderSearch from "./components/header-search/header-search";
 import HeaderAvatar from "./components/header-avatar/header-avatar";
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import Link from "next/link";
 
 export default function DashboardHeader() {
   return (
@@ -15,7 +32,49 @@ export default function DashboardHeader() {
       <div className="flex items-center gap-8">
         <Moon className="text-white" />
         <Bell className="text-white" />
-        <HeaderAvatar />
+        <DropdownMenu>
+          <DropdownMenuTrigger>
+            <HeaderAvatar />
+          </DropdownMenuTrigger>
+          <DropdownMenuContent className="w-full bg-zinc-950 text-white border-2 border-gray-600/90 p-2 ">
+            <DropdownMenuLabel className="flex gap-2">
+              <HeaderAvatar />
+              <div>
+                <h4>{"Relancio Borges"}</h4>
+                <h5 className="font-light leading-5">@{"RelancioBorgesDev"}</h5>
+              </div>
+            </DropdownMenuLabel>
+            <DropdownMenuSeparator />
+            <Link href={"/settings/profile"}>
+              <DropdownMenuItem className="flex items-center gap-2">
+                <User />
+                Perfil
+              </DropdownMenuItem>
+            </Link>
+
+            <Link href={"/settings/help"}>
+              <DropdownMenuItem className="flex items-center gap-2">
+                <MessageCircleQuestion />
+                Ajuda
+              </DropdownMenuItem>
+            </Link>
+
+            <Link href={"/settings/setting"}>
+              <DropdownMenuItem className="flex items-center gap-2">
+                <Settings />
+                Configurações
+              </DropdownMenuItem>
+            </Link>
+            <DropdownMenuSeparator />
+
+            <Link href={"/settings/setting"}>
+              <DropdownMenuItem className="flex items-center gap-2 bg-red-500 focus:bg-red-600">
+                <LogOut />
+                Sair
+              </DropdownMenuItem>
+            </Link>
+          </DropdownMenuContent>
+        </DropdownMenu>
       </div>
     </header>
   );
