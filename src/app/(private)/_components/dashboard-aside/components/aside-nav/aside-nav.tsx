f4960161bba924ed3eab07185e79ce5ad3adb687
RelancioBorgesDev <relancio.dev@gmail.com>
2024-04-16 11:39:47 -0300

    chore: ajuste no currentPath do aside-nav

--- src/app/(private)/_components/dashboard-aside/components/aside-nav/aside-nav.tsx
+++ src/app/(private)/_components/dashboard-aside/components/aside-nav/aside-nav.tsx
@@ -1,4 +1,3 @@
-import { Home } from "lucide-react";
 import Link from "next/link";
 import React, { ElementType } from "react";
 
@@ -6,12 +5,24 @@ interface AsideNavProps {
   href: string;
   icon: ElementType;
   name: string;
+  currentPath: string;
 }
 
-export default function AsideNav({ href, icon: Icon, name }: AsideNavProps) {
+export default function AsideNav({
+  href,
+  icon: Icon,
+  name,
+  currentPath,
+}: AsideNavProps) {
+  let bgStyle = "";
+  if (currentPath === href) {
+    bgStyle = "bg-white text-zinc-950";
+  }
   return (
     <Link href={href}>
-      <button className="text-white flex items-center gap-2 w-full hover:bg-zinc-900/90 py-4 px-6 cursor-pointer duration-500 ease-in-out">
+      <button
+        className={`text-white flex items-center gap-2 w-full hover:bg-zinc-400/40 py-4 px-6 cursor-pointer duration-500 ease-in-out ${bgStyle}`}
+      >
         <Icon className="" size={22} />
         <span className="text-xl">{name}</span>
       </button>
