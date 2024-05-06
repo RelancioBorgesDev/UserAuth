743774b740c4d0cffea5f11d4bf9692e8fec3126
RelancioBorgesDev <relancio.dev@gmail.com>
2024-05-06 16:51:58 -0300

    feat: add dados do usuario em settings

--- src/app/(private)/dashboard/settings/setting/page.tsx
+++ src/app/(private)/dashboard/settings/setting/page.tsx
@@ -1,9 +1,47 @@
-import React from "react";
+"use client";
+import { Input } from "@/components/input/input";
+import { Label } from "@radix-ui/react-dropdown-menu";
+import { useSession } from "next-auth/react";
+import React, { useEffect, useState } from "react";
 
 export default function Setting() {
+  const { data: session } = useSession();
+
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [userName, setUserName] = useState("");
+
+  async function retrieveUserData() {
+    if (session) {
+      if (session.user) {
+        const { name, email, userName } = session.user;
+        setName(name);
+        setEmail(email);
+        setUserName(userName);
+      }
+    }
+  }
+
+  useEffect(() => {
+    retrieveUserData();
+  }, [session]);
   return (
     <div className="text-white">
       <h1 className="text-4xl font-bold max-sm:text-center">Configurações</h1>
+      <form>
+        <div>
+          <Label>Nome</Label>
+          <Input className="text-zinc-950" type="text" value={name} />
+        </div>
+        <div>
+          <Label>Username</Label>
+          <Input className="text-zinc-950" type="text" value={userName} />
+        </div>
+        <div>
+          <Label>Email</Label>
+          <Input className="text-zinc-950" type="text" value={email} />
+        </div>
+      </form>
     </div>
   );
 }
