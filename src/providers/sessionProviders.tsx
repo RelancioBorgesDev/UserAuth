a5099018ae57f424e7c6b5564f76b6aa047fb8d1
RelancioBorgesDev <relancio.dev@gmail.com>
2024-05-06 16:46:13 -0300

    refactor: editado session provider

--- src/providers/sessionProviders.tsx
+++ src/providers/sessionProviders.tsx
@@ -1,13 +1,17 @@
 "use client";
+import { AnyNsRecord } from "dns";
+import { Session } from "next-auth";
 import { SessionProvider } from "next-auth/react";
 import { ReactNode } from "react";
 
 interface NextAuthSessionProviderProps {
   children: ReactNode;
+  session: any;
 }
 
 export default function NextAuthSessionProvider({
   children,
+  session,
 }: NextAuthSessionProviderProps) {
-  return <SessionProvider>{children}</SessionProvider>;
+  return <SessionProvider session={session}>{children}</SessionProvider>;
 }
