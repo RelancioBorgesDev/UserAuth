1fb9ab8cee60fe4ecad5cf4089c545b787182f72
RelancioBorgesDev <relancio.dev@gmail.com>
2024-05-06 16:47:19 -0300

    feat: adicionado github provider para login

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -4,6 +4,7 @@ import Credentials from "next-auth/providers/credentials";
 import { comparePasswords } from "@/utils/bcrypt";
 import { RegisterFormData } from "@/app/auth/(auth)/sign-up/_components/register-form/register-form";
 import { getUserByEmail } from "@/utils/userValidation";
+import GitHub from "next-auth/providers/github";
 
 const nextAuthOptions: NextAuthOptions = {
   providers: [
@@ -32,9 +33,22 @@ const nextAuthOptions: NextAuthOptions = {
         }
       },
     }),
+    GitHub({
+      clientId: process.env.GITHUB_CLIENT_ID!,
+      clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+      profile(profile: any) {
+        return {
+          id: profile.id.toString(),
+          name: profile.name,
+          userName: profile.login,
+          email: profile.email,
+          image: profile.avatar_url,
+        };
+      },
+    }),
   ],
   jwt: {
-    maxAge: 30 * 24 * 60 * 60, // 30 days
+    maxAge: 30 * 24 * 60 * 60,
   },
   cookies: {
     sessionToken: {
@@ -51,14 +65,19 @@ const nextAuthOptions: NextAuthOptions = {
     signIn: "/auth/sign-in",
   },
   callbacks: {
-    async jwt({ token, user }) {
+    async jwt({ token, account, user }) {
       if (user) {
         token.user = user;
+      }
+
+      if (account) {
+        token.accessToken = account.access_token;
       }
       return token;
     },
     async session({ session, token }) {
       session.user = token.user as any;
+      session.accessToken = token.accessToken as string;
       return session;
     },
   },
--- src/components/social-login-option/social-login-option.tsx
+++ src/components/social-login-option/social-login-option.tsx
@@ -1,5 +1,6 @@
+import { signIn } from "next-auth/react";
 import Link from "next/link";
-import React, { ElementType } from "react";
+import { ElementType } from "react";
 
 interface SocialLoginOptionProps {
   icon: ElementType;
@@ -12,8 +13,11 @@ export default function SocialLoginOption({
   text,
   href,
 }: SocialLoginOptionProps) {
+  const providerSignIn = async () => {
+    await signIn("github");
+  };
   return (
-    <Link href={href}>
+    <Link href={href} onClick={providerSignIn}>
       <button className="w-full flex items-center gap-4 bg-slate-100/70  py-2 px-12 rounded hover:bg-zinc-950/80 hover:text-white ease-in-out duration-100">
         <Icon size={24} />
         <span>{text}</span>
